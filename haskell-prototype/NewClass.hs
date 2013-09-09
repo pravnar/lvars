5425e169c63a4f5c8a9e509c68f385291aa8fbb8
Ryan Newton <rrnewton@gmail.com>
2013-09-09 11:29:11 -0400

    Switch to making Future as well as FutContents an associated type.

--- haskell-prototype/NewClass.hs
+++ haskell-prototype/NewClass.hs
@@ -13,13 +13,6 @@ import qualified Control.Monad.Par as MP
 import qualified Data.LVar.IVar as LI
 import qualified Control.LVish as LVish
 
--- class Monad m => ParFuture m where
-  
-  -- | The type of a future that goes along with the particular `Par`
-  -- monad the user chooses.
---   type Future a 
-
-
 
 -- | @ParFuture@ captures the class of Par monads which support
 --   futures.  This level of functionality subsumes @par@/@pseq@ and is
@@ -28,13 +21,17 @@ import qualified Control.LVish as LVish
 --   A minimal implementation consists of `spawn_` and `get`.
 --   However, for monads that are also a member of `ParIVar` it is
 --   typical to simply define `spawn` in terms of `fork`, `new`, and `put`.
--- 
-class Monad m => ParFuture future m | m -> future where
+--
+class Monad m => ParFuture m where
 
+  -- | The type of a future that goes along with the particular `Par`
+  -- monad the user chooses.
+  type Future m a
+  
   -- | Different implementations may place different constraints on
   -- what is allowable inside a Future.  For example, some
   -- implementations may require an `Eq` Constraint.
-  type FutContents future a :: Constraint
+  type FutContents m a :: Constraint
 
   -- | Create a potentially-parallel computation, and return a /future/
   -- (or /promise/) that can be used to query the result of the forked
@@ -45,18 +42,18 @@ class Monad m => ParFuture future m | m -> future where
   -- >    fork (p >>= put r)
   -- >    return r
   --
-  spawn  :: (NFData a, FutContents future a) => m a -> m (future a)
+  spawn  :: (NFData a, FutContents m a) => m a -> m (Future m a)
 
   -- | Like 'spawn', but the result is only head-strict, not fully-strict.
-  spawn_ :: FutContents future a => m a -> m (future a)
+  spawn_ :: FutContents m a => m a -> m (Future m a)
 
-  -- | Wait for the result of a future, and then return it.
-  get    :: future a -> m a
+  -- | Wait for the result of a Future, and then return it.
+  get    :: Future m a -> m a
 
   -- | Spawn a pure (rather than monadic) computation.  Fully-strict.
   --
   -- >  spawnP = spawn . return
-  spawnP :: (NFData a, FutContents future a) =>   a -> m (future a)
+  spawnP :: (NFData a, FutContents m a) =>   a -> m (Future m a)
 
   -- Default implementations:
   spawn  p = spawn_ (do x <- p; deepseq x (return x))
@@ -68,8 +65,9 @@ test1 = MP.runPar $ do
   MP.get x
 
 -- We achieve backwards compatibility simply by putting in a null constraint:
-instance ParFuture MP.IVar MP.Par  where
-  type FutContents MP.IVar a = ()
+instance ParFuture MP.Par where
+  type Future      MP.Par a = MP.IVar a 
+  type FutContents MP.Par a = ()
   get    = MP.get
   spawn  = MP.spawn
   spawn_ = MP.spawn_
@@ -80,8 +78,9 @@ test2 = MP.runPar $ do
   x <- spawn $ return "hello"
   get x
 
-instance ParFuture (LI.IVar s) (LVish.Par d s) where
-  type FutContents (LI.IVar s) a = (Eq a)
+instance ParFuture (LVish.Par d s) where
+  type Future      (LVish.Par d s) a = LI.IVar s a   
+  type FutContents (LVish.Par d s) a = (Eq a)
   spawn_ m = LI.spawn_ m
   get iv = LI.get iv
 
@@ -89,4 +88,3 @@ test3 :: String
 test3 = LVish.runPar $ do
   x <- spawn $ return "hello"
   get x
-
